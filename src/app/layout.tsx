1f653afa290e5afffb3af0c18a08c29814433e09
MatheusFerreira33 <matheus.fsilva33@gmail.com>
2023-11-29 19:37:21 -0300

    feat:config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,28 @@
+import { Inter } from 'next/font/google';
+import { ChakraProvider } from '@chakra-ui/react';
+import StyledComponentsRegistry from './lib/registry';
+import { GlobalStyle } from '../style/Global';
 
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className} style={{
+        background:'red',
+        display:'none',
+        margin:'0',
+        boxSizing:'border-box'
+      }}>
+        <ChakraProvider>
+          <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+        </ChakraProvider>
+      </body>
+    
     </html>
-  )
+  );
 }
